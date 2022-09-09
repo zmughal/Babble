b9c175bc8b0809dee1469271bc768854c6e16cdc
Zakariyya Mughal <zaki.mughal@gmail.com>
2022-09-09 12:05:26 -0400

    Bump version to 0.090008

--- lib/Babble.pm
+++ lib/Babble.pm
@@ -2,7 +2,7 @@ package Babble;
 
 use strictures 2;
 
-our $VERSION = '0.090007_03';
+our $VERSION = '0.090008';
 
 1;
 
@@ -18,7 +18,7 @@ Babble - EXPERIMENTAL Babel-like for perl
 
 =head1 VERSION
 
-0.009001
+0.090008
 
 =head1 SYNOPSIS
 
